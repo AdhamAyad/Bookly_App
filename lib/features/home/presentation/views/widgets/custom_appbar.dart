64d01200f25f9d1721500c274d8abd352aa37171
Adham Ayad <aboayad557@gmail.com>
2023-10-18 20:03:04 +0200

    add custom appbar to home view

---
+++ lib/features/home/presentation/views/widgets/custom_appbar.dart
@@ -0,0 +1,23 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40,horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo,scale: 3.7,), //todo: or make hight to image = 16
+         //todo: const spacer(), to make space between
+          IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.magnifyingGlass,size: 23,))
+    
+        ],
+      ),
+    );
+  }
+}
