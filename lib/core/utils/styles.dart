90e3153939054684fdad45af0ef7d4cada7e5147
Adham Ayad <aboayad557@gmail.com>
2023-10-23 21:12:00 +0200

    comment on styels

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 abstract class Styles {
   // todo : make styles of text here with size that used more than one time and named it with font size and make it bold if the greater is bold
   //? named by font size and when we reuse it we use copyWith() to edit and append
-
+   //! Make style for reapeted size and that has the majority in font weight
   //? text style that size is 18 and semibold
   static const textStyle18 =TextStyle(fontSize: 18,
   fontWeight: FontWeight.w600);
