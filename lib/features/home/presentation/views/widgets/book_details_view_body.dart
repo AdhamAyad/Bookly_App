015cd18d0180b08b417b0acb8fd1fd22eab5f4f2
Adham Ayad <aboayad557@gmail.com>
2023-10-26 00:11:08 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -16,57 +16,63 @@ class BookdetailsViewBody extends StatelessWidget {
         .width; //? to be responsive with other devices
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Text(
-            'The Jungle Book',
-            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            //? Opacity → to make opicaty for text شفافيه اللون
-            opacity: .7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(
-                fontWeight: FontWeight.w500,
-                fontStyle:
-                    FontStyle.italic, /*color: Colors.white.withOpacity(.7)*/
+      child: CustomScrollView( //? make it scrolable and able me to scroll when using expanded widget
+        slivers: [
+          SliverFillRemaining( //? SliverFillRemaining → fill every empty space in scrollview
+          hasScrollBody: false, //? لان كداكدا هيتعملها اسكرول ب الكامل
+            child: Column(
+          children: [
+            const CustomBookDetailsAppBar(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * .2),
+              child: const CustomBookImage(),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'The Jungle Book',
+              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Opacity(
+              //? Opacity → to make opicaty for text شفافيه اللون
+              opacity: .7,
+              child: Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontStyle:
+                      FontStyle.italic, /*color: Colors.white.withOpacity(.7)*/
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-
-          const SizedBox(height: 37,),
-
-          const BooksAction(),
-
-          const SizedBox(height: 43,),
-
-          Align( //? align → to put it only in centerleft
-            alignment: Alignment.centerLeft,
-            child: Text('You May Also Like',style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w900),)),
-
-            const SizedBox(height: 16,),
-
-            const SimilerBooksListView(),
+            const SizedBox(
+              height: 16,
+            ),
+            const BookRating(
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+      
+            const SizedBox(height: 37,),
+      
+            const BooksAction(),
             
-        ],
+            //? Expanded → make it to be responsive with another devices and take all space it able to take
+            const Expanded(child:  SizedBox(height: 43,)), 
+      
+            Align( //? align → to put it only in centerleft
+              alignment: Alignment.centerLeft,
+              child: Text('You May Also Like',style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w900),)),
+      
+              const SizedBox(height: 16,),
+      
+              const SimilerBooksListView(),
+              
+          ],
+        ),)] 
       ),
     );
   }
