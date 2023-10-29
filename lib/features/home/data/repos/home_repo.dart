505ea81f0588ab3a3e6e77e517b452b6b1915cde
Adham Ayad <aboayad557@gmail.com>
2023-10-29 12:03:31 +0200

    make failure file use dartz package

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+//? Either<left,right> → right: if it goes good without errors , left: if goes error return that
 abstract class HomeRepo //! class that have all method that we will use 
 { //! repos pattern → make abstract class that we iside it make the featuers and methods that we will make and that tell what it will do?
-  fetchBestSellerBooks(); // todo: can make in abstract class only → must be rewrite in classes that extendes from it by @override
-  fetchFeaturedBooks(); //? it uses when i want to make method with more than one way
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks(); // todo: can make in abstract class only → must be rewrite in classes that extendes from it by @override
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks(); //? it uses when i want to make method with more than one way
 }
