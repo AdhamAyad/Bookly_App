abef6722d6bbbb799914edcc16bbb03e03c11aaa
Adham Ayad <aboayad557@gmail.com>
2023-10-29 10:31:13 +0200

    make home repos

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,5 @@
+abstract class HomeRepo //! class that have all method that we will use 
+{ //! repos pattern → make abstract class that we iside it make the featuers and methods that we will make and that tell what it will do?
+  fetchBestSellerBooks(); // todo: can make in abstract class only → must be rewrite in classes that extendes from it by @override
+  fetchFeaturedBooks(); //? it uses when i want to make method with more than one way
+}
