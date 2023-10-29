6e072eaa38b9221fe76dedf7b4bf35bbf143abe8
Adham Ayad <aboayad557@gmail.com>
2023-10-29 09:33:15 +0200

    search view done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -25,21 +25,6 @@ class SearchViewBody extends StatelessWidget {
   }
 }
 
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        //shrinkWrap: true, //? take hight that the child of list view takes and build all widget in one time and same time
-       // physics: //* if i delete it it will scroll and page scroll
-           // const NeverScrollableScrollPhysics(), //? dont scroll becuse customscrollview will scroll it and it becoms same column
-        padding: EdgeInsets.zero,
-        itemCount: 10,
-        itemBuilder: ((context, index) {
-          return const BookListViewItem();
-        }));
-  }
-}
 
 
